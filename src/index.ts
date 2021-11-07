b509a1f4ece1d30b2d2ffc2b0a94cf408b97ad68
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2021-11-07 20:04:14 -0300

    prodCommit

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,12 @@ import cors from 'cors';
 import './database/connect';
 import routes from './routes';
 
-require('dotenv').config({ path: '.env' });
+import "dotenv/config"; 
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(routes);
+app.use(routes); 
 
-app.listen(process.env.PORT || 3000, () => console.log('Server started at http://localhost:3000 🚀'));
+app.listen(process.env.PORT? +process.env.PORT : 3000, () => console.log('Server started at http://localhost:3000 🚀'));
