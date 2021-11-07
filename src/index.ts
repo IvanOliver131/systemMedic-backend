fc1a6b2ef554dc973774c8945858f740cedf5dfd
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2021-11-07 20:09:20 -0300

    prodCommit

--- src/index.ts
+++ src/index.ts
@@ -13,4 +13,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes); 
 
-app.listen(process.env.PORT? +process.env.PORT : 3000, () => console.log('Server started at http://localhost:3000 🚀'));
+app.listen(process.env.PORT || 3000, () => console.log('Server started at http://localhost:3000 🚀'));
